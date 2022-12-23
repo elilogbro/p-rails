1a575c207222ffa0baecf50ec25981fb31b69c2d
Eli <+>
2022-12-23 17:33:10 -0500

    added default route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+    protect_from_forgery with::exception
+    def not_found_method
+      render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :projects, only: [:index]
+
+  match '*unmatched', to: 'application#not_found_method', via: :all
+
 end
